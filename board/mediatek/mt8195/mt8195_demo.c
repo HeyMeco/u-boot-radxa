68837fa29355120177d4077a368ed3440788423e
Howard Lin <howard-yh.lin@mediatek.com>
2024-10-07 18:32:48 +0800

    GENIO: board: mt8195: add mtd support for firmware updates
    This allows the mtd device to process firmware updates.
    
    Change-Id: I5566c39dc58bb39dd5d6e4ca9b9b9f439381f01c
    Signed-off-by: Howard Lin <howard-yh.lin@mediatek.com>

--- board/mediatek/mt8195/mt8195_demo.c
+++ board/mediatek/mt8195/mt8195_demo.c
@@ -18,7 +18,10 @@
 static struct efi_fw_image fw_images[MT8195_UPDATABLE_IMAGES] = {0};
 
 struct efi_capsule_update_info update_info = {
-#if defined(CONFIG_UFS_MEDIATEK)
+#if IS_ENABLED(CONFIG_DFU_MTD)
+	.dfu_string = "mtd nor0=bl2.img part 1;"
+			"fip.bin part 2;firmware.vfat part 4;u-boot-env.bin part 9",
+#elif defined(CONFIG_UFS_MEDIATEK)
 	.dfu_string = "ufs 0=bl2.img raw 0x0 0x400 dev 0;"
 			"fip.bin part 2 1;firmware.vfat part 2 3;u-boot-env.bin raw 0x0 0x400 dev 1",
 #else
@@ -56,6 +59,12 @@ static bool board_is_genio_1200_evk_ufs(void)
 {
 	return CONFIG_IS_ENABLED(TARGET_MT8195) &&
 		of_machine_is_compatible("mediatek,genio-1200-evk-ufs");
+}
+
+static bool board_is_genio_1200_evk_ufs_qspi(void)
+{
+	return CONFIG_IS_ENABLED(TARGET_MT8195) &&
+		of_machine_is_compatible("mediatek,genio-1200-evk-ufs-qspi");
 }
 
 void mediatek_capsule_update_board_setup(void)
@@ -120,6 +129,24 @@ void mediatek_capsule_update_board_setup(void)
 		fw_images[2].fw_name = u"GENIO-1200-EVK-UFS-BL2";
 		fw_images[3].fw_name = u"GENIO-1200-EVK-UFS-FW";
 		fw_images[4].fw_name = u"GENIO-1200-EVK-UFS-ENV";
+	} else if (board_is_genio_1200_evk_ufs_qspi()) {
+		efi_guid_t image_type_guid = GENIO_1200_EVK_UFS_QSPI_FIT_IMAGE_GUID;
+		efi_guid_t uboot_image_type_guid = GENIO_1200_EVK_UFS_QSPI_FIP_IMAGE_GUID;
+		efi_guid_t bl2_image_type_guid = GENIO_1200_EVK_UFS_QSPI_BL2_IMAGE_GUID;
+		efi_guid_t fw_image_type_guid = GENIO_1200_EVK_UFS_QSPI_FW_IMAGE_GUID;
+		efi_guid_t env_image_type_guid = GENIO_1200_EVK_UFS_QSPI_ENV_IMAGE_GUID;
+
+		guidcpy(&fw_images[0].image_type_id, &image_type_guid);
+		guidcpy(&fw_images[1].image_type_id, &uboot_image_type_guid);
+		guidcpy(&fw_images[2].image_type_id, &bl2_image_type_guid);
+		guidcpy(&fw_images[3].image_type_id, &fw_image_type_guid);
+		guidcpy(&fw_images[4].image_type_id, &env_image_type_guid);
+
+		fw_images[0].fw_name = u"GENIO-1200-EVK-UFS-QSPI-FIT";
+		fw_images[1].fw_name = u"GENIO-1200-EVK-UFS-QSPI-FIP";
+		fw_images[2].fw_name = u"GENIO-1200-EVK-UFS-QSPI-BL2";
+		fw_images[3].fw_name = u"GENIO-1200-EVK-UFS-QSPI-FW";
+		fw_images[4].fw_name = u"GENIO-1200-EVK-UFS-QSPI-ENV";
 	}
 }
 #endif /* CONFIG_EFI_HAVE_CAPSULE_SUPPORT && CONFIG_EFI_PARTITION */
